4b0176f28cb0a6d7dbc0bb96f49432a821aa5e25
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-16 12:29:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,29 @@
-class Song
+class Song # creating class called "Song"
+
+  #attribute accessor for name and artist_name 
   attr_accessor :name, :artist_name
+  
+  #creates a class variable of an empty array
   @@all = []
 
+  #defines a Song-class method of "all" that returns @@all which is all the songs 
   def self.all
     @@all
   end
 
+  #defines an instance method "save" which pushes the instance of  "Song") into... ____ ?!?! what?!   
   def save
     self.class.all << self
   end
+  
+  def find_or_create_by_name(name)
+  end 
 
+  
 end
+
+s = Song.new #creates a new song "s" from the Song class
+s.name = "The Ship Song" #defines what the name of the song is (but where is the method for this!?!?)
+s.artist_name = "Nick Cave"  #defines what the name of the artist of the song is (but where is the method for this!?!?)
+s.save #saves the song, in theory
+
